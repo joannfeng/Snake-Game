1e2af6f9f55fc63c85296849ee11107e4eb11b13
Joann Feng <joannfeng@wireless-10-148-220-150.public.utexas.edu>
2021-01-21 00:21:48 -0600

    added patterns

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = 5; // how many times snake moves per second
+export const SNAKE_SPEED = 6; // how many times snake moves per second
 const snakeBody = [{ x: 11, y: 11}];
 let newSegments = 0;
 
